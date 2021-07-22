aacfd393e8aee0fffd5b6f663de0563ff5f3d394
Ben Mitchinson <mitchinson.dev@gmail.com>
2021-07-21 22:07:40 -0500

    Capacitor XCode Init Build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.mitchinson.haptic_test',
+  appName: 'haptic-test',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
